0db32ffcbec85ffa58c682177c57d004701ee344
Muhammad Achar <muhammadacharzardari12@gmail.com>
2026-01-22 01:31:56 +0500

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const io = new Server(server, {
     methods: ["GET","POST"],
     credentials: true
   },
-  transports: ["polling","websocket"] // allow fallback
+  transports: ["websocket"] // allow fallback
 });
 
 const emailSockettoMapping = new Map();
